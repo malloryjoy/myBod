afda18aea693db676aae1376b83e83dba4996ec3
Mallory Joy <malloryjoy13@gmail.com>
2021-09-11 13:56:47 -0400

    Added Modal for results display

--- workmeout.js
+++ workmeout.js
@@ -40,15 +40,41 @@ function theWorkout (){
     var workoutPlaylist = 'https://open.spotify.com/playlist/3xAYxdp7A7SmZ54o0sJUMT';
     var workoutGuide = 'https://open.spotify.com/playlist/3xAYxdp7A7SmZ54o0sJUMT';
   }
-    
-    console.log("Your workout today is \n" + yourWorkout + "\n" + workoutDescription);
-  myBodContainer.innerHTML = `<div class="yay"><h3>Your workout today is</h3><br />${yourWorkout}<br /><br />${workoutDescription}<br /><a href="${workoutPlaylist}">Open Targeted Workout Playlist</a> | <a href="${workoutGuide}">Open Basic Workout Guide</a></div>`; //framework for results display
+    var myWorkout = yourWorkout.toUpperCase();
+    console.log("Your workout today is \n" + myWorkout + "\n" + workoutDescription);
+  myBodContainer.innerHTML = `<h3>Your workout today is</h3><br />${myWorkout}<br /><br />${workoutDescription}<br /><a href="${workoutPlaylist}">Open Targeted Workout Playlist</a> | <a href="${workoutGuide}">Open Basic Workout Guide</a>`; //framework for results display
   }
-  const myBodContainer = document.getElementById('myBod');
+  const myBodContainer = document.getElementById('myBod'); //displays the workout results
    
   
-  const hitTheNos = document.getElementById('workitout');
-  hitTheNos.addEventListener('click', theWorkout);
+  const genWorkout = document.getElementById('workitout'); //assigns the workout generator to the Work Me Out button
+  genWorkout.addEventListener('click', theWorkout); //initates the generation of the workout when the button is clicked 
+
+// Get the modal
+var modal = document.getElementById("theproj");
+
+// Get the button that opens the modal
+var btn = document.getElementById("workitout");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
   
     
   
